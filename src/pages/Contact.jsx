6af681c12de7c178bddf920380bf9ab39f8cea15
özcan çetin <cetin445@gmail.com>
2022-08-06 14:01:33 +0300

    add github, linkedin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     <article className="d-flex justify-content-center contact gap-2 flex-column flex-sm-row flex-wrap align-items-center">
       <div className="text-light d-flex justify-content-center align-items-center contactLinkedin p-4 ">
         <Link
-          to="https://www.linkedin.com/in/ozcan-cetin/"
+          to="//www.linkedin.com/in/ozcan-cetin/"
           target="_blank"
         >
           <span className="display-2 d-flex align-items-center rounded-3">
@@ -21,18 +21,18 @@ const Contact = () => {
         </Link>
       </div>
       <div className="text-light text-light  d-flex justify-content-center align-items-center contactGithub p-4">
-        <Link to="https://github.com/ozcan-cetin" target="_blank">
+        <Link to="//github.com/ozcan-cetin" target="_blank">
         <span className="display-2 d-flex align-items-center rounded-circle">
           <BsGithub />
         </span>
         </Link>
       </div>
       <div className=" text-light  d-flex justify-content-center align-items-center contactEmail p-4  ">
-        <a href="mailto:cetin445@gmail.com" target="_blank">
+      <Link to="mailto:cetin445@gmail.com" target="_blank">
         <span className="display-2 d-flex align-items-center justify-content-center ">
           <BsFillEnvelopeFill/>
         </span>
-        </a>
+        </Link>
 
       </div>
     </article>
